fc65189e6bcc16ecdad21395df574c27fadad0e1
Habibulloh <habibulloh@example.com>
2025-12-05 23:44:34 +0500

    proxy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher , types
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.memory import MemoryStorage
 from config import settings
 from buttons.button_main import rt
@@ -8,9 +9,11 @@ from registration import rtr
 
 logging.basicConfig(level=logging.INFO)
 
+PROXY_URL = "http://proxy.server:3128"
+session = AiohttpSession(proxy=PROXY_URL)
 BOT_TOKEN = settings.BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN , session=session)
 
 dp = Dispatcher(storage=MemoryStorage())
 
